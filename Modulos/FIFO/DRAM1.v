176726f21adf0dd2c72a8b58aa33dfa88a63b8ed
Ricardo Soro <sorojara@Umbreon>
2018-06-13 10:13:50 -0500

    DRAM1

--- Modulos/FIFO/DRAM1.v
+++ Modulos/FIFO/DRAM1.v
@@ -1,12 +1,12 @@
 module true_dpram_sclk
 (
-	input [7:0] data_a, data_b,
+	input [7:0] data_a,
 	input [5:0] addr_a, addr_b,
-	input we_a, we_b, clk,
-	output reg [7:0] q_a, q_b
+	input we_a, re_b, clk,
+	output reg [7:0] q_b
 );
 	// Declare the RAM variable
-	reg [7:0] ram[63:0];
+	reg [3:0] ram[7:0];
 	
 	// Port A
 	always @ (posedge clk)
@@ -14,29 +14,20 @@ module true_dpram_sclk
 		if (we_a) 
 		begin
 			ram[addr_a] <= data_a;
-			q_a <= data_a;
+			
 		end
-		else 
-		begin
-			q_a <= ram[addr_a];
-		end
+	
 	end
 	
 	// Port B
 	always @ (posedge clk)
 	begin
-		if (we_b)
-		begin
-			ram[addr_b] <= data_b;
-			q_b <= data_b;
-		end
-		else
+		if (re_b)
 		begin
 			q_b <= ram[addr_b];
 		end
+	
 	end
 endmodule
 
-//agregar enable; hardcodear un we_ en 0 y el otro 1
-	
-endmodule
+
