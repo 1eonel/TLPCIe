887cbb05ea2da17455db1a6fa34db4e55e1f0068
Ricardo Soro <sorojara@Umbreon>
2018-06-20 14:40:29 -0500

    DRAM correccion

--- Modulos/FIFO/DRAM/TBDRam.v
+++ Modulos/FIFO/DRAM/TBDRam.v
@@ -1,3 +1,16 @@
 `timescale 1ns/1ps
 
 `include "DRAM1.v"
+
+module testDRAM1 ();
+
+  reg clk;
+  reg we_a;
+  reg re_b;
+  reg [5:0] addr_a;
+  reg [5:0] addr_b;
+  reg [7:0] data_a;
+  wire [7:0] q_b;
+
+  DRAM1 ram(.data_a(data_a), .addr_a(addr_a), .addr_b(addr_b), .we_a(we_a), .re_b(re_b), .clk(clk), .q_b(q_b));
+endmodule
