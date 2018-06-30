758df082271d7965bde09bbc802478b8952d8278
Ricardo Soro <sorojara@Umbreon>
2018-06-30 16:31:45 -0500

    FIFO counter arreglado

--- Modulos/FIFO/FIFO.v
+++ Modulos/FIFO/FIFO.v
@@ -17,95 +17,102 @@ module FIFO
 	DRAM1 ram(.data_a(data_a), .addr_a(contadorW), .addr_b(contadorR), .we_a(push), .re_b(pop), .clk(clk), .q_b(q_b));
 
 
-always @ (posedge clk) begin
-	if (reset == 0) begin
-		contadorR <= 4'd0000;
-		vueltaR <= 4'd0000;
-		empty <= 1;
-		full <= 0;
-		almost_full <= 0;
-		almost_empty <= 0;
-		error <= 0;
-		contadorW <= 0;
-		vueltaW <= 0;
-	end else begin
-		if(contadorR==contadorW) begin
-			if(vueltaR==vueltaW) begin
-				empty <= 1;
-				full <= 0;
-				almost_full <= 0;
-				almost_empty <= 0;
-				error <= 0;
-			end else begin
-				empty <= 0;
-				full <= 1;
-				almost_full <= 0;
-				almost_empty <= 0;
-				error <= 0;
-			end
-		end else if(contadorR>contadorW)begin
-			if(vueltaR==vueltaW) begin
-				empty <= 0;
-				full <= 0;
-				almost_full <= 0;
-				almost_empty <= 0;
-				error <= 1;
-			end else begin
-				empty <= 0;
-				full <= 0;
-				error <= 0;
-				if (contadorW + 8 - contadorR<=1) begin
-					almost_empty <= 1;
-					almost_full <= 0;
-				end else if (contadorW + 8 - contadorR >= 6) begin
-					almost_empty <= 0;
-					almost_full <= 1;
-				end else begin
-					almost_empty <= 0;
-					almost_full <= 0;
-				end
-			end
+	always @ (posedge clk) begin
+		if (reset == 1) begin
+			contadorR <= 4'b0000;
+			vueltaR <= 0;
+			empty <= 1;
+			full <= 0;
+			almost_full <= 0;
+			almost_empty <= 0;
+			error <= 0;
+			contadorW <= 4'b0000;
+			vueltaW <= 0;
 		end else begin
-			if(vueltaR==vueltaW) begin
-				empty <= 0;
-				full <= 0;
-				error <= 0;
-				if (contadorW - contadorR<=1) begin
-					almost_empty <= 1;
+			if(contadorR==contadorW) begin
+				if(vueltaR==vueltaW) begin
+					empty <= 1;
+					full <= 0;
 					almost_full <= 0;
-				end else if (contadorW - contadorR >= 6) begin
 					almost_empty <= 0;
-					almost_full <= 1;
+					error <= 0;
 				end else begin
-					almost_empty <= 0;
+					empty <= 0;
+					full <= 1;
 					almost_full <= 0;
+					almost_empty <= 0;
+					error <= 0;
+				end
+			end else if(contadorR>contadorW)begin
+				if(vueltaR==vueltaW) begin
+					empty <= 0;
+					full <= 0;
+					almost_full <= 0;
+					almost_empty <= 0;
+					error <= 1;
+				end else begin
+					empty <= 0;
+					full <= 0;
+					error <= 0;
+					if (contadorW + 8 - contadorR<=1) begin
+						almost_empty <= 1;
+						almost_full <= 0;
+					end else if (contadorW + 8 - contadorR >= 6) begin
+						almost_empty <= 0;
+						almost_full <= 1;
+					end else begin
+						almost_empty <= 0;
+						almost_full <= 0;
+					end
 				end
 			end else begin
-				empty <= 0;
-				full <= 0;
-				almost_full <= 0;
-				almost_empty <= 0;
-				error <= 1;
+				if(vueltaR==vueltaW) begin
+					empty <= 0;
+					full <= 0;
+					error <= 0;
+					if (contadorW - contadorR<=1) begin
+						almost_empty <= 1;
+						almost_full <= 0;
+					end else if (contadorW - contadorR >= 6) begin
+						almost_empty <= 0;
+						almost_full <= 1;
+					end else begin
+						almost_empty <= 0;
+						almost_full <= 0;
+					end
+				end else begin
+					empty <= 0;
+					full <= 0;
+					almost_full <= 0;
+					almost_empty <= 0;
+					error <= 1;
+				end
 			end
 		end
 	end
-end
 
 
-always @ (posedge clk) begin
-	if (push) begin
-		// if (contadorW == 7) begin
-		// 	contadorW <= 0;
-		// end else begin
-		// 	contadorW <= contadorW+1;
-		// end
-		contadorW <= contadorW == 7 ? 0 : contadorW+4'd0001;
+	always @ (posedge clk) begin
+		if (push) begin
+			if (contadorW == 7) begin
+				contadorW <= 0;
+				vueltaW <= vueltaW == 1 ? 0 : 1;
+			end else begin
+				contadorW <= contadorW+4'b0001;
+			end
+			// contadorW <= contadorW == 4'b0111 ? 0 : contadorW+4'b0001;
+		end
+
+		if (pop) begin
+			if (contadorR == 7) begin
+				contadorR <= 0;
+				vueltaR <= vueltaR == 1 ? 0 : 1;
+			end else begin
+				contadorR <= contadorR+4'b0001;
+			end
+			//contadorR <= contadorR == 7 ? 0 : contadorR+1;
+		end
 	end
-
-	if (pop) begin
-		contadorR <= contadorR == 7 ? 0 : contadorR+1;
-	end
-end
 
 
 
